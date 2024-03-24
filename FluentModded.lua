fc2779a135677e197966d7496c8a818225e9d6c5
meta-trashmethod <163018386+meta-trashmethod@users.noreply.github.com>
2024-03-24 15:02:16 +0700

    Create FluentModded.lua

---
+++ FluentModded.lua
@@ -0,0 +1,72 @@
+local HttpService = game:GetService("HttpService")
+local LocalPlayer = game:GetService("Players").LocalPlayer
+
+local bb="TEST"
+
+
+local FluentModded = {} do
+
+    FluentModded.Ignore = {}
+    FluentModded.Folder = "Anastassy"
+    FluentModded.Settings = {
+        SettingsInterfaceTheme = "Aqua",
+        SettingsInterfaceTransparency = true,
+        SettingsInterfaceMenuKeybind = "Home",
+    }
+
+    function FluentModded:Setup(Library)
+        self.Library = Library
+        self.Options = Library.Options
+
+        if not isfolder(self.Folder) then
+            makefolder(self.Folder)
+        end
+
+        if not isfile(self.Folder .. "/" .. bb .. "_" .. LocalPlayer.Name .. ".cfg") then
+            writefile(self.Folder .. "/" .. bb .. "_" .. LocalPlayer.Name .. ".cfg", "[]");
+        end
+    end
+
+    function FluentModded:Save()
+        local Config = {};
+
+        for Idx, Option in next, self.Options do
+            if (not table.find(self.Ignore, Idx)) then
+                if Option.Type then
+                    Config[Idx] = Option.Type == "Colorpicker" and {Option.Value, Option.Transparency} or Option.Type == "Keybind" and {Option.Value, Option.Mode} or Option.Value
+                end
+            end
+        end
+
+        writefile(self.Folder .. "/" .. bb .. "_" .. LocalPlayer.Name .. ".cfg", HttpService:JSONEncode(Config));
+    end
+
+    function FluentModded:Load()
+        local Config = HttpService:JSONDecode(readfile(self.Folder .. "/" .. bb .. "_" .. LocalPlayer.Name .. ".cfg"));
+
+        for Option, Value in next, Config do
+            if self.Options[Option] and (not table.find(self.Ignore, Option)) then
+                self.Options[Option]:SetValue(Value);
+            end
+        end
+    end
+
+    function FluentModded:Build(Tab)
+        local Library = self.Library
+        
+        local Section = {
+            [1] = Tab:AddSection("Interface"),
+            [2] = Tab:AddSection("Configuration")
+        }
+
+        Section[1]:AddDropdown("SettingsInterfaceTheme", { Title = "Theme", Description = "Changes the interface theme.", Values = Library.Themes, Default = self.Settings.SettingsInterfaceTheme }):OnChanged(function(Value) self.Library:SetTheme(Value) self.Settings.SettingsInterfaceTheme = Value self:Save("Theme") end)
+        Section[1]:AddToggle("SettingsInterfaceTransparency", { Title = "Transparency", Description = "Makes the interface transparent.", Default = self.Settings.SettingsInterfaceTransparency }):OnChanged(function(Value) self.Library:SetTransparency(Value) self.Settings.SettingsInterfaceTransparency = Value self:Save("Theme") end)
+        Section[1]:AddKeybind("SettingsInterfaceMenuKeybind", { Title = "Minimize Bind", Default = self.Settings.SettingsInterfaceMenuKeybind }):OnChanged(function(Value) self.Settings.SettingsInterfaceMenuKeybind = Value self:Save("Theme") end)
+
+        self.Library.MinimizeKeybind = self.Options.SettingsInterfaceMenuKeybind
+    end
+
+    
+end
+
+return FluentModded
